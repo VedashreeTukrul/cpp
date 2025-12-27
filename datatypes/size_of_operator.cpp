8f32b43c566d24d6d347599e9b85250d6b275623
vedashree_tukrul <tukrulvedashree52@gmail.com>
2025-12-27 21:14:03 +0530

    adding size of operator file

---
+++ datatypes/size_of_operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int marks = 10;
+	
+	cout << sizeof(marks)<< endl;
+	cout << sizeof(float)<< endl;
+	cout << sizeof(double)<< endl;
+	cout << sizeof(char)<< endl;
+	cout << sizeof(bool)<< endl;
+	cout << sizeof(int)<< endl;
+}
